607c401d88c91d31ce8fcbd9fac784f7e742904e
Anton Safonov <help>
2024-02-23 19:34:20 +0500

    plan edit moved to modal

--- core/views.py
+++ core/views.py
@@ -630,77 +630,35 @@ def add_plan_entry_form(request):
         return render(request, 'core/partials/plan_entry_form.html', context)
 
 
-@login_required(login_url='login_user')
-@allowed_user_roles(['ADMIN', 'MODERATOR'])
-def plans_delete(request, pk):
-    # NOT NEEDED
-    Order.objects.get(pk=pk).delete()
-    orders = Order.objects.all().order_by('-id')
-    context = {
-        'orders': orders,
-    }
-    return render(request, 'core/partials/orders_list.html', context)
-
-
-@login_required(login_url='login_user')
-@allowed_user_roles(['ADMIN', 'MODERATOR'])
-def plan_entries_delete(request, r_pk, re_pk):
-    OrderEntry.objects.get(pk=re_pk).delete()
-    order = Order.objects.get(pk=r_pk)
-    context = {
-        'order': order,
-    }
-    return render(request, 'core/partials/order_entries_list.html', context)
-
-
 def plan_modal(request):
-    pk = int(str(request.GET.get("pk")))
-    plan = Plan.objects.get(pk=pk)
-    context = {
-        'plan': plan
-    }
-    return render(request, 'core/partials/plan_modal.html', context)
+    if request.method == 'GET':
+        pk = int(str(request.GET.get("pk")))
+        plan = Plan.objects.get(pk=pk)
+        form = PlanForm(instance=plan)
+        formset = PlanEntryFormset(instance=plan)
+        context = {
+            'plan': plan,
+            'form': form,
+            'formset': formset,
+        }
+        return render(request, 'core/partials/plan_modal.html', context)
+    if request.method == 'POST':
+        pk = int(str(request.POST.get("pk")))
+        plan = Plan.objects.get(pk=pk)
+        form = PlanForm(request.POST, instance=plan)
+        if form.is_valid():
+            plan_instance = form.save(commit=False)
+            plan_instance.save()
 
-
-def test(request):
-    print(request)
-    form = PlanForm(initial={
-        'date': datetime.datetime.now().strftime("%Y-%m-%dT%H:%M"),
-        'machine': Machine.objects.get(name='Турок')
-    })
-    context = {
-        "form": form,
-    }
-    return render(request, 'test.html', context=context)
-
-
-def test_save(request):
-    plan, created = Plan.objects.get_or_create(machine_id=int(request.POST.get('machine')),
-                                               date=request.POST.get('date'),
-                                               )
-    form = PlanForm(request.POST, instance=plan)
-    if form.is_valid():
-        form.save()
-    return HttpResponse('saved')
-
-
-def plan_cell_form(request):
-    plan = Plan.objects.get(pk=request.GET.get('pk'))
-    form = PlanForm(instance=plan)
-    context = {
-        'form': form,
-    }
-    return render(request, 'core/partials/plan_cell_form.html', context=context)
-
-
-def plan_cell_save(request):
-    plan, created = Plan.objects.get_or_create(machine_id=int(request.POST.get('machine')),
-                                               date=request.POST.get('date'),
-                                               )
-    form = PlanForm(request.POST, instance=plan)
-    if form.is_valid():
-        form.save()
-    context = {
-        'plan': plan,
-    }
-    return render(request, 'core/partials/plan_cell.html', context=context)
+            entry_formset = PlanEntryFormset(request.POST, request.FILES, instance=plan_instance)
+            # print(entry_formset.is_valid())
+            # print(entry_formset.errors)
+            if entry_formset.is_valid():
+                for entry_form in entry_formset:
+                    if entry_form.cleaned_data['DELETE'] is not True:
+                        entry_form.save()
+                    else:
+                        # print(entry_form.cleaned_data)
+                        if entry_form.cleaned_data['id'] is not None:
+                            entry_form.cleaned_data['id'].delete()
+        return HttpResponse('', headers={'HX-Refresh': 'true'})
